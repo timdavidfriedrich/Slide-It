7df4d4249ec3bb47ec6c7c3532e7f1cd041174a7
Tim David Friedrich <timdavidfriedrich@gmail.com>
2023-05-27 12:03:02 +0200

    removed unnecessary import

--- lib/features/ratings/screens/add_screen.dart
+++ lib/features/ratings/screens/add_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:log/log.dart';
 import 'package:provider/provider.dart';
 import 'package:rating/constants/constants.dart';
 import 'package:rating/constants/global.dart';
