d743945ace03df923cc9dbe07c2f4886163f2da0
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2023-08-24 08:48:40 +0200

    [ADD] button to copy group id

--- lib/features/social/widgets/group_invitation_dialog.dart
+++ lib/features/social/widgets/group_invitation_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:qr_flutter/qr_flutter.dart';
@@ -11,6 +12,17 @@ class GroupInvitationDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void copyGroupId() async {
+      await Clipboard.setData(ClipboardData(text: group.id));
+      if (!context.mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Gruppen-ID wurde in die Zwischenablage kopiert."),
+          duration: Duration(seconds: 5),
+        ),
+      );
+    }
+
     void close() {
       context.pop();
     }
@@ -38,12 +50,26 @@ class GroupInvitationDialog extends StatelessWidget {
             ),
           ),
           const SizedBox(height: Constants.smallPadding),
-          SelectableText(group.id),
+          Row(
+            children: [
+              Flexible(
+                child: SelectableText(
+                  group.id,
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              const SizedBox(width: Constants.smallPadding),
+              IconButton(
+                icon: const Icon(Icons.copy),
+                onPressed: copyGroupId,
+              ),
+            ],
+          ),
         ],
       ),
       actions: [
         PlatformDialogAction(
-          onPressed: () => close(),
+          onPressed: close,
           child: const Text("Schließen"),
         ),
       ],
